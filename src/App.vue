27dfc1fbee895a353dab82347721149bd65e8c4c
Vladislav Pavlov <vladpavlov9957@gmail.com>
2020-06-15 05:03:25 +0300

    updated main component

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,100 @@
 <template>
     <v-app>
-        <v-app-bar app color="primary" dark>
-            <div class="d-flex align-center">
-                <v-img
-                    alt="Vuetify Logo"
-                    class="shrink mr-2"
-                    contain
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-                    transition="scale-transition"
-                    width="40"
-                />
+        <Navbar></Navbar>
 
-                <v-img
-                    alt="Vuetify Name"
-                    class="shrink mt-1 hidden-sm-and-down"
-                    contain
-                    min-width="100"
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-                    width="100"
-                />
-            </div>
+        <v-main>
+            <v-container>
+                <router-view />
+            </v-container>
+        </v-main>
 
-            <v-spacer></v-spacer>
-
-            <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-                <span class="mr-2">Latest Release</span>
-                <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
-        </v-app-bar>
-
-        <v-content></v-content>
+        <delete-dialog
+            v-if="deleteDialogState"
+            :state="deleteDialogState"
+            :title="deleteDialogTitle"
+            :description="deleteDialogDescription"
+            @dialog="closeDeleteDialog"
+        ></delete-dialog>
     </v-app>
 </template>
 
 <script lang="ts">
-import Vue from "vue"
+import Vue from 'vue'
+import eventBus from './eventBus'
+import Navbar from '@/components/Navbar.vue'
+import DeleteDialog from '@/components/DeleteDialog.vue'
 
 export default Vue.extend({
     name: "App",
-
-    components: {},
-
+    components: {
+        Navbar,
+        DeleteDialog
+    },
     data: () => ({
-        //
-    })
+        deleteDialogId: '',
+        deleteDialogCaller: 'ROOT',
+        deleteDialogState: false,
+        deleteDialogTitle: 'Warning',
+        deleteDialogDescription: 'Are you sure?',
+    }),
+    created() {
+        this.$store.commit('loadState')
+        this.setVuetifyDarkTheme(this.dark)
+    },
+    mounted() {
+        eventBus.$on('DELETE_DIALOG_OPEN', this.openDeleteDialog)
+        eventBus.$on('DELETE_DIALOG_CLOSE', this.closeDeleteDialog)
+    },
+    computed: {
+        dark() {
+            return this.$store.getters.dark
+        }
+    },
+    watch: {
+        dark(value: boolean) {
+            this.setVuetifyDarkTheme(value)
+        }
+    },
+    methods: {
+        setVuetifyDarkTheme(dark: boolean) {
+            this.$vuetify.theme.dark = dark
+        },
+        openDeleteDialog(options: {
+            id: string,
+            caller: string,
+            title?: string,
+            description?: string
+        }) {
+            const opts = options
+            this.deleteDialogId = opts.id
+            this.deleteDialogState = true
+            this.deleteDialogCaller = opts.caller.toUpperCase()
+            if (opts.title) this.deleteDialogTitle = opts.title
+            if (opts.description) this.deleteDialogDescription = opts.description
+        },
+        closeDeleteDialog(dialog: boolean) {
+            eventBus.$emit(
+                `${this.deleteDialogCaller}_DELETE_DIALOG_RESULT`,
+                this.deleteDialogId,
+                dialog
+            )
+
+            this.deleteDialogId = ''
+            this.deleteDialogCaller = 'ROOT'
+            this.deleteDialogState = false
+            this.deleteDialogTitle = 'Warning'
+            this.deleteDialogDescription = 'Are you sure?'
+        }
+    }
 })
 </script>
+
+<style lang="scss">
+.w-100 {
+    width: 100%;
+}
+
+.h-100 {
+    height: 100%;
+}
+</style>
