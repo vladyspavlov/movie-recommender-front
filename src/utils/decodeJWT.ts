9f2c3b2edffc8d8f9a505a083ce96372b576093b
Vladislav Pavlov <vladpavlov9957@gmail.com>
2020-06-15 05:00:25 +0300

    added util for decode jwt token

---
+++ src/utils/decodeJWT.ts
@@ -0,0 +1,16 @@
+export function decodeJWT(token: string) {
+    const base64Url = token.split('.')[1]
+    const base64 = base64Url
+        .replace(/-/g, '+')
+        .replace(/_/g, '/')
+    const jsonPayload = decodeURIComponent(
+        atob(base64)
+            .split('')
+            .map(c => {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+            })
+            .join('')
+    )
+
+    return JSON.parse(jsonPayload)
+}
