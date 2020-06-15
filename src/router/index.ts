bf5ff2e9f9411fddceb159cafa9523582c09c780
Vladislav Pavlov <vladpavlov9957@gmail.com>
2020-06-15 05:00:44 +0300

    router update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,80 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
     {
         path: '/',
-        name: 'Home',
-        component: () => import('../views/Home.vue')
+        name: 'home',
+        component: () => import('@/views/Home.vue'),
+        beforeEnter: (to, from, next) => {
+            if (store.getters.token) {
+                next({ name: 'profile' })
+            } else {
+                next()
+            }
+        }
+    },
+    {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/Profile.vue'),
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/movie/:id',
+        name: 'movie',
+        component: () => import('@/views/Movie.vue'),
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/person/:id',
+        name: 'person',
+        component: () => import('@/views/Person.vue'),
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/popular',
+        name: 'popular',
+        component: () => import('@/views/Popular.vue'),
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/search',
+        name: 'search',
+        component: () => import('@/views/Search.vue'),
+        meta: {
+            requiresAuth: true
+        }
     }
 ]
 
 const router = new VueRouter({
     mode: 'history',
-    base: process.env.BASE_URL,
+    base: '/',
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!store.getters.token) {
+            next({ name: 'home' })
+        } else {
+            next()
+        }
+    } else {
+        next()
+    }
 })
 
 export default router
